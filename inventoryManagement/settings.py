775fa85922a9405d27e232958246859d47c8d4dd
VishalCR7 <vishaliitr7@gmail.com>
2017-10-21 14:32:01 +0530

    added allowed host

--- inventoryManagement/settings.py
+++ inventoryManagement/settings.py
@@ -26,7 +26,7 @@ LOGIN_REDIRECT_URL = 'main:index'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
